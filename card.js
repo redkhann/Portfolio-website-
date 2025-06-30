7b25e45136c2a64c53fd1690f8ec251c05719d85
redkhann <rehan.khan.786092.rk@gmail.com>
2025-06-30 11:21:29 +0530

    Delete card.js

--- card.js
+++
@@ -1,2 +0,0 @@
-export function Card({ children, className }) { return <div className={`rounded-lg shadow-md bg-white ${className}`}>{children}</div>; }
-export function CardContent({ children, className }) { return <div className={className}>{children}</div>; }
